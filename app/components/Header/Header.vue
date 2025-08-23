e3dc45ea632e42a3ef864de5b05cc9f8aaacbd55
mert2kk <mertkadirkayan@gmail.com>
2025-08-23 23:07:17 +0300

    header is responsive

--- app/components/Header/Header.vue
+++ app/components/Header/Header.vue
@@ -1,28 +1,32 @@
 <template>
   <div
-    class="w-[900px] flex flex-row items-center bg-[#131b16] bg-opacity-80 rounded-b-full xSmall:flex-col"
+    class="w-[900px] flex items-center bg-[#131b16] bg-opacity-80 rounded-b-full customSm:flex-col"
   >
     <div class="mx-auto">
-      <img src="/images/logo.png" alt="logo" class="w-[90px] h-[90px]" />
+      <img
+        src="/images/logo.png"
+        alt="logo"
+        class="w-[90px] h-[90px] customSm:w-[50px] customSm:h-[50px]"
+      />
     </div>
     <ul class="flex space-x-4 justify-center w-[40%] items-center">
-      <li class="text-[#EBEBEB] font-bold text-sm">Timer</li>
-      <li class="text-[#EBEBEB] font-bold text-sm">Report</li>
-      <li class="text-[#EBEBEB] font-bold text-sm">Tasks</li>
-      <li class="text-[#EBEBEB] font-bold text-sm">My Chain</li>
+      <li class="text-[#EBEBEB] font-bold text-sm customSm:text-xs">Timer</li>
+      <li class="text-[#EBEBEB] font-bold text-sm customSm:text-xs">Report</li>
+      <li class="text-[#EBEBEB] font-bold text-sm customSm:text-xs">Tasks</li>
+      <li class="text-[#EBEBEB] font-bold text-sm customSm:text-xs">MyChain</li>
     </ul>
-    <button
-      class="text-[#EBEBEB] flex mr-3 justify-center font-bold text-xs px-2 py-1 rounded-lg hover:bg-[#EBEBEB] hover:text-[#131b16]"
-    >
-      <GlobeAsiaAustraliaIcon class="w-4 h-4" />
-    </button>
 
-    <div class="mr-auto">
+    <div class="mx-auto flex items-center customSm:mt-2">
       <button
         class="text-[#EBEBEB] flex justify-center font-semibold text-xs bg-[#0a120d] px-6 py-2 rounded-xl hover:bg-[#5e6863] hover:scale-105 transition-all"
       >
         <UserIcon class="w-4 h-4 inline mr-2" />
         Sign In
+      </button>
+      <button
+        class="text-[#EBEBEB] flex ml-3 justify-center font-bold text-xs px-2 py-1 rounded-lg hover:bg-[#EBEBEB] hover:text-[#131b16]"
+      >
+        <GlobeAsiaAustraliaIcon class="w-4 h-4" />
       </button>
     </div>
   </div>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,12 +11,13 @@ const config: Config = {
     theme: {
         extend: {
             screens: {
-                xxSmall: { raw: "(max-width: 400px)" }, // custom width breakpoint
-                xSmall: { raw: "(max-width: 780px)" }, // custom height breakpoint
-                Small: { raw: "(max-width: 940px)" }, // custom height breakpoint
-                md: { raw: "(min-width: 16000px)" }, // custom height breakpoint
-                lg: { raw: "(min-width: 1920)" }, // custom height breakpoint
-                xl: { raw: "(min-width: 2160px)" }, // custom height breakpoint
+                xxSmall: { raw: "(max-width: 400px)" },
+                xSmall: { raw: "(max-width: 780px)" },
+                Small: { raw: "(max-width: 940px)" },
+                md: { raw: "(min-width: 16000px)" },
+                lg: { raw: "(min-width: 1920)" },
+                xl: { raw: "(min-width: 2160px)" },
+                customSm: { raw: "(max-width: 600px)" }
             },
         },
     },
