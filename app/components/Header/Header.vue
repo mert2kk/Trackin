aa162b6f0bf8e0c1a09721cf0e516c3674cbf5e0
mert2kk <mertkadirkayan@gmail.com>
2025-08-23 21:11:17 +0300

    css is fixed

--- app/components/Header/Header.vue
+++ app/components/Header/Header.vue
@@ -1,27 +1,21 @@
 <template>
   <div
-    class="w-[55%] flex flex-row items-center p-6 bg-[#131b16] bg-opacity-80 rounded-b-full"
+    class="w-[900px] flex flex-row items-center bg-[#131b16] bg-opacity-80 rounded-b-full"
   >
     <div class="mx-auto">
-      <img
-        src="/images/logo.png"
-        alt="logo"
-        class="w-[100px] h-[100px] my-auto absolute top-2 bottom-2"
-      />
+      <img src="/images/logo.png" alt="logo" class="w-[100px] h-[100px]" />
     </div>
-    <ul class="flex space-x-4 justify-start w-[40%] mx-auto items-center">
+    <ul class="flex space-x-4 justify-center w-[40%] items-center">
       <li class="text-[#EBEBEB] font-bold text-sm">Timer</li>
       <li class="text-[#EBEBEB] font-bold text-sm">Report</li>
       <li class="text-[#EBEBEB] font-bold text-sm">Tasks</li>
       <li class="text-[#EBEBEB] font-bold text-sm">My Chain</li>
     </ul>
-    <v-divider vertical class="mx-4" />
     <button
-      class="text-[#EBEBEB] flex justify-center font-bold text-xs px-2 py-1 rounded-xl hover:bg-[#3d3d3d]"
+      class="text-[#EBEBEB] flex mr-3 justify-center font-bold text-xs px-2 py-1 rounded-xl hover:bg-[#3d3d3d]"
     >
       <GlobeAsiaAustraliaIcon class="w-4 h-4" />
     </button>
-    <v-divider vertical class="mx-4" />
 
     <div class="mr-auto">
       <button
